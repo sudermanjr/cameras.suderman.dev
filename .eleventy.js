7657ae4961e30f412299d43441621f23704ef001
Andrew Suderman <andrew@sudermanjr.com>
2021-11-20 21:01:17 -0700

    Session 14 complete

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
-
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
 module.exports = config => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
+
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
 
   // Returns work items, sorted by display order
   config.addCollection('work', collection => {
@@ -20,6 +26,13 @@ module.exports = config => {
   // Returns a collection of blog posts in reverse date order
   config.addCollection('blog', collection => {
     return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
+
+  // Returns a list of people ordered by filename
+  config.addCollection('people', collection => {
+    return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+      return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+    });
   });
 
   return {

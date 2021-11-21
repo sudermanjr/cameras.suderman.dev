1aa4e7725ea2566dd1387a484692620e5358cc4d
Andrew Suderman <andrew@sudermanjr.com>
2021-11-20 22:15:15 -0700

    idk

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,9 @@ module.exports = config => {
     });
   });
 
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
